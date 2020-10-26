42747f1b1ecac7a4c2902def3696f5d40c7ec6fb
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-10-26 18:28:55 +0000

    eslint-prettier-editorconfig

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello World</h1>
+    </>
+  );
+};
+
+export default App;
